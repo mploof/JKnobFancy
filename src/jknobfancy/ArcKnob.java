3f1a2cbbdbe408d72286f5efa6efc8fc0877af3b
Michael Ploof <michael@dynamicperception.com>
2016-03-01 13:39:29 -0500

    Updated ArcKnob Javadoc

--- src/jknobfancy/ArcKnob.java
+++ src/jknobfancy/ArcKnob.java
@@ -9,9 +9,18 @@ import java.awt.geom.Point2D;
 
 import javax.swing.ImageIcon;
 
-@SuppressWarnings("serial")
+/**
+ * This class extends JKnobFancy by drawing an arc between two handles. If the object is
+ * initialized with three handles, the arc will be drawn between the second and third handles. The
+ * intent of this is that the first handle would represent a pointer and the second and third 
+ * handles would represent a range on the knob.
+ * 
+ * @author Michael Ploof
+ *
+ */
 public class ArcKnob extends JKnobFancy{
 
+	private static final long serialVersionUID = 3629098161796965896L;
 	private int initHandles;
 	private Color arcColor;
 	private double arcStartDeg;
@@ -27,48 +36,62 @@ public class ArcKnob extends JKnobFancy{
 	
 	/**
 	 * Three handle constructor
-	 * @param initDeg0
-	 * @param handleIcon0
-	 * @param initDeg1
-	 * @param handleIcon1
-	 * @param initDeg2
-	 * @param handleIcon2
-	 * @param relCenter
-	 * @param relTrackRadius
-	 * @param backgroundIcon
-	 * @param backgroundWidth
+	 * @param initDeg0 handle 0 initial position in degrees
+	 * @param handleIcon0 handle 0 ImageIcon
+	 * @param initDeg1 handle 1 initial position in degrees
+	 * @param handleIcon1 handle 1 ImageIcon
+	 * @param initDeg2 handle 2 initial position in degrees
+	 * @param handleIcon2 handle 2 ImageIcon
+	 * @param relCenter Point2D containing values 0.0 to 1.0 representing position of the center of 
+	 * 		rotation relative to the background image
+	 * @param relTrackRadius value 0.0 to 1.0 representing size of handle track relative to the
+	 * 		width of the background image 
+	 * @param backgroundIcon ImageIcon for background
+	 * @param backgroundWidth desired width in pixels of background image
 	 */
-	public ArcKnob(double initDeg0, ImageIcon handleIcon0, double initDeg1, ImageIcon handleIcon1, double initDeg2,ImageIcon handleIcon2, 
-			Point2D relCenter, float relTrackRadius, ImageIcon backgroundIcon, int backgroundWidth) {
-		this.init(initDeg0, handleIcon0, initDeg1, handleIcon1, initDeg2, handleIcon2, relCenter, relTrackRadius, backgroundIcon, backgroundWidth);		
+	public ArcKnob(double initDeg0, ImageIcon handleIcon0, double initDeg1, ImageIcon handleIcon1, 
+			double initDeg2,ImageIcon handleIcon2, Point2D relCenter, float relTrackRadius, 
+			ImageIcon backgroundIcon, int backgroundWidth) {
+		this.init(initDeg0, handleIcon0, initDeg1, handleIcon1, initDeg2, handleIcon2, relCenter, 
+				relTrackRadius, backgroundIcon, backgroundWidth);		
 	}
 	
 	/**
 	 * Two handle constructor
-	 * @param initDeg0
-	 * @param handleIcon0
-	 * @param initDeg1
-	 * @param handleIcon1
-	 * @param relCenter
-	 * @param relTrackRadius
-	 * @param backgroundIcon
-	 * @param backgroundWidth
+	 * @param initDeg0 handle 0 initial position in degrees
+	 * @param handleIcon0 handle 0 ImageIcon
+	 * @param initDeg1 handle 1 initial position in degrees
+	 * @param handleIcon1 handle 1 ImageIcon
+	 * @param relCenter Point2D containing values 0.0 to 1.0 representing position of the center of 
+	 * 		rotation relative to the background image
+	 * @param relTrackRadius value 0.0 to 1.0 representing size of handle track relative to the
+	 * 		width of the background image 
+	 * @param backgroundIcon ImageIcon for background
+	 * @param backgroundWidth desired width in pixels of background image
 	 */
 	public ArcKnob(double initDeg0, ImageIcon handleIcon0, double initDeg1, ImageIcon handleIcon1, 
-			Point2D relCenter, float relTrackRadius, ImageIcon backgroundIcon, int backgroundWidth) {
-		this.init(initDeg0, handleIcon0, initDeg1, handleIcon1, relCenter, relTrackRadius, backgroundIcon, backgroundWidth);		
+			Point2D relCenter, float relTrackRadius, ImageIcon backgroundIcon, 
+			int backgroundWidth) {
+		this.init(initDeg0, handleIcon0, initDeg1, handleIcon1, relCenter, relTrackRadius, 
+				backgroundIcon, backgroundWidth);		
 	}
 	
-	public void init(double initDeg0, ImageIcon handleIcon0, double initDeg1, ImageIcon handleIcon1, double initDeg2,ImageIcon handleIcon2, Point2D relCenter, float relTrackRadius, ImageIcon backgroundIcon, int backgroundWidth) {
-		super.init(initDeg0, relCenter, relTrackRadius, backgroundIcon, backgroundWidth, handleIcon0);		
+	public void init(double initDeg0, ImageIcon handleIcon0, double initDeg1, ImageIcon handleIcon1,
+			double initDeg2,ImageIcon handleIcon2, Point2D relCenter, float relTrackRadius, 
+			ImageIcon backgroundIcon, int backgroundWidth) {
+		super.init(initDeg0, relCenter, relTrackRadius, backgroundIcon, backgroundWidth, 
+				handleIcon0);		
 		this.addHandle(initDeg1, handleIcon1);
 		this.addHandle(initDeg2, handleIcon2);
 		this.initHandles = 3;
 		setDefaultArcColor();
 	}
 	
-	public void init(double initDeg0, ImageIcon handleIcon0, double initDeg1, ImageIcon handleIcon1, Point2D relCenter, float relTrackRadius, ImageIcon backgroundIcon, int backgroundWidth) {
-		super.init(initDeg0, relCenter, relTrackRadius, backgroundIcon, backgroundWidth, handleIcon0);
+	public void init(double initDeg0, ImageIcon handleIcon0, double initDeg1, ImageIcon handleIcon1,
+			Point2D relCenter, float relTrackRadius, ImageIcon backgroundIcon, 
+			int backgroundWidth) {
+		super.init(initDeg0, relCenter, relTrackRadius, backgroundIcon, backgroundWidth, 
+				handleIcon0);
 		this.addHandle(initDeg1, handleIcon1);
 		this.initHandles = 2;
 		setDefaultArcColor();
@@ -120,10 +143,12 @@ public class ArcKnob extends JKnobFancy{
 		}
 		if(handles.size() >= 2 && initHandles >= 2){
 			arcStartDeg = (int)handles.get(startHandle).getAngleDeg();
-			arcSizeDeg = (int)handles.get(stopHandle).getAngleDeg() - (int)handles.get(startHandle).getAngleDeg(); 
+			arcSizeDeg = (int)handles.get(stopHandle).getAngleDeg() - 
+					(int)handles.get(startHandle).getAngleDeg(); 
 			arcSizeDeg = arcSizeDeg < 0 ? 360 + arcSizeDeg : arcSizeDeg;			
 			int handleDia = this.getHandle(0).getRadius() * 2;
-			g2.fillArc(center.x - trackRadius + handleDia, center.y - trackRadius + handleDia, (trackRadius - handleDia) * 2, (trackRadius - handleDia) * 2, 
+			g2.fillArc(center.x - trackRadius + handleDia, center.y - trackRadius + handleDia, 
+					(trackRadius - handleDia) * 2, (trackRadius - handleDia) * 2, 
 					(int)arcStartDeg, (int)arcSizeDeg);
 		}
 	}
